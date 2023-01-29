899498f6ae806b9e34154b48d8968d9af997d356
xavier <xavimg@blanquerna.url.edu>
2023-01-29 14:42:33 +0100

    last fixes

--- Makefile
+++ Makefile
@@ -2,4 +2,5 @@ run:
 	docker-compose up --build
 
 test:
+	go clean -testcache
 	go test -v ./...
--- listener/internal/server/ws_listener_test.go
+++ listener/internal/server/ws_listener_test.go
@@ -3,10 +3,7 @@ package server
 import (
 	"net/http"
 	"net/http/httptest"
-	"strings"
 	"testing"
-
-	"github.com/gorilla/websocket"
 )
 
 func TestHTTPUpgradedToWebSocket(t *testing.T) {
@@ -29,48 +26,5 @@ func TestHTTPUpgradedToWebSocket(t *testing.T) {
 
 	if status := rr.Code; status != 200 {
 		t.Errorf("Wrong status code: got %v, want %v", status, 200)
-	}
-}
-
-func TestWebSocketConnection(t *testing.T) {
-	s := httptest.NewServer(http.HandlerFunc(echo))
-	defer s.Close()
-
-	u := "ws" + strings.TrimPrefix(s.URL, "http")
-
-	ws, _, err := websocket.DefaultDialer.Dial(u, nil)
-	if err != nil {
-		t.Fatalf("%v", err)
-	}
-	defer ws.Close()
-
-	if err := ws.WriteMessage(websocket.TextMessage, []byte("test-message")); err != nil {
-		t.Fatalf("%v", err)
-	}
-	_, p, err := ws.ReadMessage()
-	if err != nil {
-		t.Fatalf("%v", err)
-	}
-
-	if string(p) != "test-message" {
-		t.Fatalf("bad message")
-	}
-}
-
-func echo(w http.ResponseWriter, r *http.Request) {
-	c, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		return
-	}
-	defer c.Close()
-	for {
-		mt, message, err := c.ReadMessage()
-		if err != nil {
-			break
-		}
-		err = c.WriteMessage(mt, message)
-		if err != nil {
-			break
-		}
 	}
 }
